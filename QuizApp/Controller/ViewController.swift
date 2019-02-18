cfc5167a153a6b77170ff5bc0cc4085214106c34
Cristian <chris.sedano.arenas@gmail.com>
2019-02-18 17:05:32 +0000

    Interface Done
    Implement two Views,three labels and two bottons

---
+++ QuizApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Cristian Sedano Arenas on 12/2/19.
+//  Copyright © 2019 Cristian Sedano Arenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
